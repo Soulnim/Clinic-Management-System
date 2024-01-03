2cfcd39d36474f884c8b5ca47f460dd3e239cd33
Luqman Khailmi <111408610+Soulnim@users.noreply.github.com>
2024-01-03 21:28:30 +0800

    Update Appointment.java

--- Latest/Appointment.java
+++ Latest/Appointment.java
@@ -12,13 +12,15 @@ public class Appointment
     private String docID;
     private String date;
     private String time;
+    private String status;
     
-    public Appointment(String appID,String patID,String docID,String date,String time) {
+    public Appointment(String appID,String patID,String docID,String date,String time,String status) {
         this.appID=appID;
         this.docID=docID;
         this.patID=patID;
         this.date=date;
         this.time=time;
+        this.status=status;
     }
     
     // appID is restricted from any changes
@@ -32,20 +34,26 @@ public class Appointment
     public String getDocID() { return this.docID; }
     public String getDate() { return this.date; }
     public String getTime() { return this.time; }
+    public String getStatus() { return this.status; }
     
     // for unformatted data inspecting
     public String toString() {
+        return " App ID : "+this.appID+" | Date : "+this.date+" | Time : "+this.time+" | Status : "+this.status;
+    }
+    public String toStringAll() {
         return " App ID : "+this.appID+" | Pat ID : "+this.patID+" | Doc ID : "+this.docID
-            +" | Date : "+this.date+" | Time : "+this.time;
+            +" | Date : "+this.date+" | Time : "+this.time+" | Status : "+this.status;
     }
     // for displaying data in beautified format
     public String toStringFormatted() {
         // Patient and Doctor's data is not local
         return " Appointment ID : "+this.appID+
-            "\n Date : "+this.date+" | Time : "+this.time;
+            "\n Date : "+this.date+" | Time : "+this.time+
+            "\n Status : "+this.status;
     }
     // for storing data to clinicdata.txt
     public String txtFormat() {
-        return "APPOINTMENT;"+this.appID+";"+this.patID+";"+this.docID+";"+this.date+";"+this.time;
+        return "APPOINTMENT;"+this.appID+";"+this.patID+";"+this.docID+";"+this.date+
+            ";"+this.time+";"+this.status;
     }
 }
